4c5e65eac96e9b882e4c6ffa4ea4c4b0b9873103
ILya-Zukhbaya <ne_perepletov@mail.ru>
2022-05-21 18:36:53 +0300

    Adding new tasks and two buttons (DEMO)

---
+++ main.js
@@ -0,0 +1,34 @@
+const todoSchedule = document.querySelector('.ToDoSchedule')
+const todoForm = document.querySelector('.ToDoForm-container')
+const todoInput = document.querySelector('#Get-Task-Text')
+
+todoForm.addEventListener('submit', formHandler)
+
+function formHandler(event) {
+    event.preventDefault(); // Отмена стандартного поведения
+    
+    const textInput = todoInput.value
+
+    const newTask = document.createElement('li')
+
+    newTask.innerText = textInput
+
+    const addButton = document.createElement('button')
+
+    addButton.setAttribute('role', 'button')
+    addButton.className = 'completeText'
+    addButton.innerText = 'Completed'
+
+    const deleteButton = document.createElement('button')
+
+    deleteButton.setAttribute('role', 'button')
+    deleteButton.className = 'removedText'
+    deleteButton.innerText = 'Remove'
+
+    newTask.append(addButton)
+    newTask.append(deleteButton)
+    todoSchedule.append(newTask)
+
+    todoInput.value = null
+    todoInput.focus()
+}
